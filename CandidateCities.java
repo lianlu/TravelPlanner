ef5b2eb418d3740a3c1af65452353f91f15177b5
gentlebreeza <Jason@Jason-Lis-MacBook-Air.local>
2015-09-27 22:52:19 -0400

    get city matrix from citymatrix

--- CandidateCities.java
+++ CandidateCities.java
@@ -4,9 +4,9 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class CandidateCities {
-	private static int[][] cities;
-	private static Map<Integer, String> map;
-	private static Map<String, Integer> map_r;
+	private  int[][] cities;
+	private  Map<Integer, String> map;
+	private  Map<String, Integer> map_r;
 	
 	public CandidateCities() {
 		//map = HashBiMap.create();
@@ -16,12 +16,14 @@ public class CandidateCities {
 	
 	public void setCostAndMap(String s) {
 		if(s.equals("east")) {
-			cities = new int[][]{
-										{0,104,112,193,134},
-										{104,0,139,113,160},
-										{112,99,0,236,188},
-										{221,161,351,0,285},
-										{156,160,188,285,0}};
+			CityMatrix cm = new CityMatrix(s);
+			cities = cm.getPrice();
+//			cities = new int[][]{
+//										{0,104,112,193,134},
+//										{104,0,139,113,160},
+//										{112,99,0,236,188},
+//										{221,161,351,0,285},
+//										{156,160,188,285,0}};
 			map.clear();
 			map.put(0, "New York");
 			map.put(1, "Boston");
@@ -37,13 +39,15 @@ public class CandidateCities {
 			map_r.put("chicago", 4);
 			
 		} else if(s.equals("west")) {
-			cities = new int[][]{
-										{0,141,204,130,177,186},
-										{130,0,151,92,134,161},
-										{280,158,0,122,184,157},
-										{130,187,155,0,252,192},
-										{169,134,144,176,0,123},
-										{150,161,122,136,123,0}};
+			CityMatrix cm = new CityMatrix(s);
+			cities = cm.getPrice();
+//			cities = new int[][]{
+//										{0,141,204,130,177,186},
+//										{130,0,151,92,134,161},
+//										{280,158,0,122,184,157},
+//										{130,187,155,0,252,192},
+//										{169,134,144,176,0,123},
+//										{150,161,122,136,123,0}};
 			map.clear();
 			map.put(0, "Los Angeles");
 			map.put(1, "San Francisco");
